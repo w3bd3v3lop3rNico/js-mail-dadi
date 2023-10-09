b9294b9351ff17a3bfadf58409b7ac3f9164c17b
Nicola Tabai <nicola.tabai98@gmail.com>
2023-10-09 16:21:32 +0200

    Add addresses array js

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # Mail
 
+- Dichiarare una variabile array
 - Chiedere la mail dell'utente
 - Verificare che la mail sia nel nel nostro database
 - Generare un messaggio appropriato in base alla verifica
---
+++ js/app.js
@@ -0,0 +1,12 @@
+// - Dichiarare una variabile array
+const mailAddresses = [
+    "pincopallo.67@gmail.com",
+    "mailrandom@alice.com",
+    "nome.cognome@hotmail.com",
+    "peter.parker@domain.com",
+    "pippopluto@gmail.com"]
+
+// - Chiedere la mail dell'utente
+
+// - Verificare che la mail sia nel nel nostro database
+// - Generare un messaggio appropriato in base alla verifica
