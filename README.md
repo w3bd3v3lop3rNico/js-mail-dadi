27fc8cb367a13f164d82bf898785cbf4b7e835b7
Nicola Tabai <nicola.tabai98@gmail.com>
2023-10-09 17:30:47 +0200

    message js inner in HTML

--- README.md
+++ README.md
@@ -3,4 +3,7 @@
 - Dichiarare una variabile array
 - Chiedere la mail dell'utente
 - Verificare che la mail sia nel nel nostro database
+    - Dichiarare una variabile che determini la lunghezza dell'array
+    - Inizializzare un ciclo per verificare se la mail inserita corrisponde o no al nostro array
+
 - Generare un messaggio appropriato in base alla verifica
--- js/app.js
+++ js/app.js
@@ -1,12 +1,39 @@
 // - Dichiarare una variabile array
+
 const mailAddresses = [
     "pincopallo.67@gmail.com",
     "mailrandom@alice.com",
     "nome.cognome@hotmail.com",
     "peter.parker@domain.com",
-    "pippopluto@gmail.com"]
+    "pippopluto@gmail.com"
+];
+console.log(mailAddresses);
 
 // - Chiedere la mail dell'utente
 
+const userMail = prompt("inserisci la tua mail");
+
 // - Verificare che la mail sia nel nel nostro database
+    // - Dichiarare una variabile che determini la lunghezza dell'array
+const arrayLength = mailAddresses.length;
+console.log(arrayLength);
+
+let message = "mail errata";
+
+    // - Inizializzare un ciclo per verificare se la mail inserita corrisponde o no al nostro array
+for (i = 0; i < arrayLength; i++) {
+    const currentMail = mailAddresses[i]
+    console.log(i, currentMail)
+    // se la user mail è uguale a una delle mail nel ciclo
+    if (userMail === currentMail) {
+        message = "mail corretta"
+    } else{
+
+    }
+}
+
+
 // - Generare un messaggio appropriato in base alla verifica
+console.log(message)
+const messageDOMElement = document.getElementById("message")
+messageDOMElement.innerHTML = message
