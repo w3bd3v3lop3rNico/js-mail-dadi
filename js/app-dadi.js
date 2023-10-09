56bf8a41f67d95967f7994a090a53c3790aeab53
Nicola Tabai <nicola.tabai98@gmail.com>
2023-10-09 20:33:12 +0200

    Dadi js and html

---
+++ js/app-dadi.js
@@ -0,0 +1,28 @@
+
+// - Generare un numero random da 1 a 6 per il giocatore e per il computer
+const btnPlayDOMElemnt = document.querySelector(".play").addEventListener('click', function () {
+
+
+    let minNumber = 1
+    const maxNumber = 6
+
+
+    const comRandomNr = Math.floor(Math.random() * (maxNumber - minNumber + 1) + minNumber);
+    const gamblerRandomNr = Math.floor(Math.random() * (maxNumber - minNumber + 1) + minNumber);
+    console.log(minNumber, maxNumber, comRandomNr, gamblerRandomNr)
+
+    let message 
+    // - Confrontare i due risultati
+    if (comRandomNr > gamblerRandomNr) {
+    message = "computer win"
+    } else if (comRandomNr < gamblerRandomNr) {
+    message = "you win"
+    } else {
+    message = "parità"
+    }
+    console.log(message)
+    // - Decretare il vincitore
+    const resultPlayDOMElement = document.getElementById('result')
+    resultPlayDOMElement.innerHTML = message
+
+})
